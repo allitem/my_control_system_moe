4f5bb3b3688199d3deefcffe62bf68bf253fbafc
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-01-30 22:48:59 +0700

    Add files via upload

--- examples/moe_integration_example.py
+++ examples/moe_integration_example.py
@@ -1,1 +1,3 @@
-
+import requests
+response = requests.post("http://localhost:5000/run_pipeline", json={"repo": "moe_repo", "version": "3.7"})
+print(response.json())
--- examples/run_default.py
+++ examples/run_default.py
@@ -1,1 +1,2 @@
-
+from control_system.controller import run_pipeline_version
+run_pipeline_version("myrepo")
